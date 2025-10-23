5c6fdeb40edf3000f7c0deb2459026032ffe02dc
junasliang <jun942670@gmail.com>
2025-10-23 17:31:25 +0800

    re-Init the repo

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	DBSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQuries *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error
+
+	testDB, err = sql.Open(dbDriver, DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQuries = New(testDB)
+	os.Exit(m.Run())
+}
